d9209d6e922ec774ece1a5c352d5236626bd5b5c
Aleksandr Butakov <abutakov84@gmail.com>
2021-03-13 20:23:00 +0300

    Refactoring: Hibernate -> JPA, DAO -> JPARepository,  config->application.config

--- src/main/java/mello/cbrcalc/entity/ValRatePK.java
+++ src/main/java/mello/cbrcalc/entity/ValRatePK.java
@@ -1,14 +1,17 @@
 package mello.cbrcalc.entity;
 
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ValRatePK implements Serializable {
-    String valuta_id;
+    String valutaId;
     LocalDate date;
 }
