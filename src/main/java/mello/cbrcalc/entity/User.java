ecaea5bfab438fda21139d3602f05674931ea87e
Aleksandr Butakov <abutakov84@gmail.com>
2021-03-16 13:22:07 +0300

    Add constraiint to registration

--- src/main/java/mello/cbrcalc/entity/User.java
+++ src/main/java/mello/cbrcalc/entity/User.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
@@ -24,16 +26,37 @@ public class User implements UserDetails {
     int id;
 
     @Column(unique = true, name = "username")
-    @Size(min = 2)
+    @NotBlank(message = "Username must be not blank")
+    @Size(min = 2, max = 15, message = "Username must be between 2 and 15 symbols")
+    @Pattern(regexp = "[_\\w\\d]*", message = "username can contain only 'a-zA-Z0-9_' symbols")
     String username;
+
     @Column(name = "password")
+    @NotBlank(message = "Password must be not blank")
+    @Size(min = 4, message = "Password must be minimum 4 symbols")
     String password;
+
     @Transient
+    @NotBlank(message = "Password confirm must be not blank")
+    @Size(min = 4, message = "Password confirm must be minimum 4 symbols")
     String passwordConfirm;
+
     @Column(name = "first_name")
+    @NotBlank(message = "First name must be not blank")
+    @Size(min = 2, max = 30, message = "First name must be between 2 and 30 symbols")
     String firstName;
+
     @Column(name = "last_name")
+    @NotBlank(message = "Last name must be not blank")
+    @Size(min = 2, max = 30, message = "Last name must be between 2 and 30 symbols")
     String lastName;
+
+    @Column(name = "email")
+    @NotBlank(message = "E-mail must be not blank")
+    @Size(min = 5, max = 30, message = "E-mail must be between 5 and 30 symbols")
+    @Pattern(regexp = ".*@.*", message = "E-mail must contain @")
+    String email;
+
     @ManyToMany(fetch = FetchType.EAGER)
     Set<Role> roles;
 
